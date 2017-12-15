60ca9961992019084e9174a0c35cda3ea29cbdf2
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2017-12-15 15:17:48 +0200

    refactor: latest devDep to webpack to fix moduleId

--- app/main.ts
+++ app/main.ts
@@ -3,30 +3,28 @@ import { AppModule } from "./app.module";
 import { android as androidApp, AndroidApplication, AndroidActivityBundleEventData } from "application";
 import { ad } from "utils/utils";
 
-const context = ad.getApplicationContext();
 declare let com: any;
 
 if (androidApp) {
+    const context = ad.getApplicationContext();
+
     androidApp.on(AndroidApplication.activityCreatedEvent, function (args: AndroidActivityBundleEventData) {
         console.log("Event: " + args.eventName + ", Activity: " + args.activity + ", Bundle: " + args.bundle);
 
         let builder = new android.os.StrictMode.VmPolicy.Builder();
         android.os.StrictMode.setVmPolicy(builder.build());
 
-        upgradeSecurityProvider(); // Needed for corner cases with HTTP request using TSL on Android API19
+        // Needed for corner cases with HTTP request using TSL on Android API19
+        com.google.android.gms.security.ProviderInstaller.installIfNeededAsync(context, new com.google.android.gms.security.ProviderInstaller.ProviderInstallListener({
+            onProviderInstalled:  () => {
+                console.log("onProviderInstalled");
+            },
+            onProviderInstallFailed: (errorCode, intent) => {
+                console.log("onProviderInstalledFailed");
+                console.log("errorCode: " + errorCode);
+            }
+        }))
     });
 }
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
-
-function upgradeSecurityProvider() {
-    com.google.android.gms.security.ProviderInstaller.installIfNeededAsync(context, new com.google.android.gms.security.ProviderInstaller.ProviderInstallListener({
-        onProviderInstalled:  () => {
-            console.log("onProviderInstalled");
-        },
-        onProviderInstallFailed: (errorCode, intent) => {
-            console.log("onProviderInstalledFailed");
-            console.log("errorCode: " + errorCode);
-        }
-    }))
-}
