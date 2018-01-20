5cb0d5037c67e1d3f120399e27eef329c1e8d3b5
NickIliev <nikolay.iliev@telerik.com>
2018-01-20 10:43:23 +0200

    refactor: added permissions plugin

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import * as permissions from "nativescript-permissions";
 
 @Component({
     selector: "ns-app",
@@ -6,5 +7,18 @@ import { Component } from "@angular/core";
 })
 
 export class AppComponent {
-    
- }
+    constructor() {
+        permissions.requestPermission([
+            "android.permission.INTERNET",
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.SET_WALLPAPER",
+            "android.permission.ACCESS_NETWORK_STATE"
+        ], "I need these permissions")
+            .then((res) => {
+                console.log("Permissions granted!");
+            }).catch(() => {
+                console.log("No permissions - plan B time!");
+            });
+    }
+}
