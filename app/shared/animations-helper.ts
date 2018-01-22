065b1d55c7f32f3ea937d1da5dc87a50bfb26499
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2018-01-22 15:16:35 +0200

    refactor: improved cards animations

--- app/shared/animations-helper.ts
+++ app/shared/animations-helper.ts
@@ -1,9 +1,12 @@
 export function translateViewByXandY(view, translateFromX, translateToX, translateFromY, translateToY, duration, curve) {
     view.translateX = translateFromX;
     view.translateY = translateFromY;
+    // view.scaleX = 0;
+    // view.scaleY = 0;
 
     view.animate({
         translate: { x: translateToX, y: translateToY },
+        // scale: {x: 1, y: 1},
         duration: duration,
         curve: curve
     });
