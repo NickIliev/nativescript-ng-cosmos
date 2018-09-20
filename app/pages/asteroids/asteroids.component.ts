07adcb4a4c8a4208166702f44dac6862d98321b5
NickIliev <nikolay.iliev@telerik.com>
2018-09-21 00:35:05 +0300

    feat: asteroid checker section exapnd - sorting and sharing added

--- app/pages/asteroids/asteroids.component.ts
+++ app/pages/asteroids/asteroids.component.ts
@@ -15,10 +15,12 @@ import { AsteroidsService } from "../../services/asteroids.service";
     styleUrls: ["./asteroids.component.css"]
 })
 export class AsteroidsComponent {
+
     asteroidItems: RxObservable<Array<AsteroidItem>>;
     asteroidCount: number;
     pageTitle: string;
-    isAscending: boolean;
+    isAscending: boolean; // TODO implement different flag for complex sorting
+    isSizeAscending: boolean;
 
     private _tempArr: Array<AsteroidItem> = [];
     private _subscr: any;
@@ -34,6 +36,7 @@ export class AsteroidsComponent {
         this.asteroidCount = 0;
         this.pageTitle = "Nearby ASTEROIDS Checker";
         this.isAscending = false; // Used for proximity sort
+        this.isSizeAscending = false;
 
         this._asteroidsService.getAsteroidsData().subscribe(
             (result: AsteroidsApiData) => {
@@ -84,7 +87,7 @@ export class AsteroidsComponent {
         );
     }
 
-    proximitySort() {
+    sortByProximity() {
         if (this.isAscending) {
             this.isAscending = false;
             this.sortProximityDescending();
@@ -92,6 +95,74 @@ export class AsteroidsComponent {
             this.isAscending = true;
             this.sortProximityAscending();
         }
+    }
+
+    sortBySize() {
+        if (this.isSizeAscending) {
+            this.isSizeAscending = false;
+            this.sortSizeDescending();
+        } else {
+            this.isSizeAscending = true;
+            this.sortSizeAscending();
+        }
+    }
+
+    sortByDanger() {
+        if (this.isAscending) {
+            this.isAscending = false;
+            this.sortDangerDescending();
+        } else {
+            this.isAscending = true;
+            this.sortDangerAscending();
+        }
+    }
+
+    sortDangerAscending() {
+        //  IMPORTNAT: TheN NASA API is providng the kilometers as a string value!!!
+        // FIX the above so tht the sorting would work as expected!!!
+        this.asteroidItems.forEach(arr => {
+            arr.sort((a, b) => {
+                return a.is_potentially_hazardous_asteroid <
+                    b.is_potentially_hazardous_asteroid
+                    ? 1
+                    : -1;
+            });
+        });
+    }
+
+    sortDangerDescending() {
+        this.asteroidItems.forEach(arr => {
+            arr.sort((a, b) => {
+                return a.is_potentially_hazardous_asteroid <
+                    b.is_potentially_hazardous_asteroid
+                    ? -1
+                    : 1;
+            });
+        });
+    }
+
+    sortSizeAscending() {
+        //  IMPORTNAT: TheN NASA API is providng the kilometers as a string value!!!
+        // FIX the above so tht the sorting would work as expected!!!
+        this.asteroidItems.forEach(arr => {
+            arr.sort((a, b) => {
+                return a.estimated_diameter.meters.estimated_diameter_max <
+                    b.estimated_diameter.meters.estimated_diameter_max
+                    ? 1
+                    : -1;
+            });
+        });
+    }
+
+    sortSizeDescending() {
+        this.asteroidItems.forEach(arr => {
+            arr.sort((a, b) => {
+                return a.estimated_diameter.meters.estimated_diameter_max <
+                    b.estimated_diameter.meters.estimated_diameter_max
+                    ? -1
+                    : 1;
+            });
+        });
     }
 
     sortProximityAscending() {
@@ -119,12 +190,17 @@ export class AsteroidsComponent {
     }
 
     onShare(index) {
+
+        console.log("onShare index: ", index);
+        console.log(this._tempArr[index]);
         // TODO - form the share content to be meaningful
         // TODO UI and UX for share buttongit
         // TODO create iOS logic
         shareText(
             "Asteroid " +
                 this._tempArr[index].name +
+                " (Approx. size: "
+                + this._tempArr[index].estimated_diameter.kilometers.estimated_diameter_max.toFixed(2) + "km)" +
                 " in close proximity with Earth on " +
                 this._tempArr[
                     index
