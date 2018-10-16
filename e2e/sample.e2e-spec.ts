bae9289279d91b5fa1d8747ca3a2e42704c7f6d5
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2018-10-16 22:59:56 +0300

    chore: extend tests

--- e2e/sample.e2e-spec.ts
+++ e2e/sample.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppiumDriver, createDriver, SearchOptions } from "nativescript-dev-appium";
 import { assert } from "chai";
+import { wait } from "nativescript-dev-appium/lib/utils";
 
 describe("sample scenario", () => {
     const defaultWaitTime = 5000;
@@ -32,30 +33,78 @@ describe("sample scenario", () => {
         assert.isTrue(astronomicalDayElement != null && (await astronomicalDayElement.isDisplayed()));
     });
 
-    const sideDrawerBtn = () => {
-        const sideDrawerLocator = "android.support.v7.widget.LinearLayoutCompat";
-        return driver.findElementByClassName(sideDrawerLocator);
+    const openSidedrawer = async () => {
+        if (driver.isAndroid) {
+            const sideDrawerLocator = "android.support.v7.widget.LinearLayoutCompat";
+            const btn = await driver.findElementByClassName(sideDrawerLocator);
+            await btn.click();
+        } else {
+            try {
+                driver.touchAction
+                    .press({ x: 8, y: 80 })
+                    .wait(250)
+                    .moveTo({ x: 320, y: 80 })
+                    .wait(250)
+                    .release()
+                    .perform();
+            } catch (error) {
+                console.log("", error);
+            }
+
+        }
+    }
+
+    const closeSidedrawer = async () => {
+        if (driver.isAndroid) {
+            const sideDrawerLocator = "android.support.v7.widget.LinearLayoutCompat";
+            const btn = await driver.findElementByClassName(sideDrawerLocator);
+            await btn.click();
+        } else {
+            try {
+                driver.touchAction
+                    .press({ x: 320, y: 80 })
+                    .release()
+                    .perform();
+            } catch (error) {
+                console.log("", error);
+            }
+
+        }
     }
 
     it("open sidedrawer menu", async () => {
-        await (await sideDrawerBtn()).click();
+        await openSidedrawer();
         const showSideDrawerResult = await driver.compareScreen("side-drawer-displayed", 5, 0.01);
         assert.isTrue(showSideDrawerResult);
     });
 
     it("close sidedrawer menu", async () => {
-        await (await sideDrawerBtn()).click();
+        await closeSidedrawer();
         const showSideDrawerResult = await driver.compareScreen("side-drawer-closed", 5, 0.01);
         assert.isTrue(showSideDrawerResult);
     });
 
     it("navigate to photo of the day and back", async () => {
-        await (await sideDrawerBtn()).click();
-        await (await driver.findElementByText("Photo of the day", SearchOptions.contains)).click();
+        await openSidedrawer();
+        await driver.sleep(5000);
+
+        const navToPhotoOfTheDay = async () => {
+            if (driver.isAndroid) {
+                (await driver.findElementByText("Photo of the day")).click();
+            } else {
+                await driver.touchAction
+                    .tap({ x: 129, y: 140 })
+                    .perform();
+            }
+        }
+        await navToPhotoOfTheDay();
+        await driver.sleep(3000);
+
         const buttons = await driver.findElementsByClassName(driver.locators.button);
+        const jupeter = await driver.findElementByText("Jupiter", SearchOptions.contains);
 
         assert.isTrue(buttons.length >= 5);
-
+        assert.isTrue(await jupeter.isDisplayed());
         await driver.navBack();
 
         const showSideDrawerResult = await driver.compareScreen("side-drawer-closed", 5, 0.01);
