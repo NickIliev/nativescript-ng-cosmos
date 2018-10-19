4247d4e5f492985b0c835af9a9ede8d2b96b9571
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2018-10-19 15:43:31 +0300

    improved css styles

--- app/pages/info/info.component.ts
+++ app/pages/info/info.component.ts
@@ -1,16 +1,42 @@
 import { Component } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
 import { isAndroid } from "tns-core-modules/platform";
 import { Page } from "tns-core-modules/ui/page";
+import { openUrl } from "tns-core-modules/utils/utils";
 
 @Component({
     selector: "cosmos-info",
     moduleId: module.id,
-    templateUrl: "./info.component.html"
+    templateUrl: "./info.component.html",
+    styleUrls: ["./info.component.css"]
 })
 export class InfoComponent {
-    constructor(private _page: Page) {
+    // tslint:disable-next-line
+    public description: string = "Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.";
+
+    constructor(private _page: Page, private _routerExtensions: RouterExtensions) {
         if (isAndroid) {
             this._page.actionBarHidden = true;
         }
+    }
+
+    onNativeScriptTap() {
+        openUrl("https://docs.nativescript.org");
+    }
+
+    onNasaTap() {
+        openUrl("https://api.nasa.gov");
+    }
+
+    onApodLink() {
+        this._routerExtensions.navigate(["/apod"], {
+            transition: {
+                name: "fade",
+                duration: 300
+            }
+        });
+    }
+    onApodURL() {
+        openUrl("https://apod.nasa.gov/apod/astropix.html");
     }
 }
